689aa8abff9fef9a4ada7575712af2d7746067fc
GevorgTsaturyan <gevorgtsaturyan485@gmail.com>
2026-07-08 23:00:00 +0400

    asd

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ import { initScheduler } from './scheduler.js';
 const app = express();
 const PORT = 3001;
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174'] }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  ...(process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : []),
+];
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
 // Public routes
